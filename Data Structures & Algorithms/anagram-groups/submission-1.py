e975d5ddf6920bd7b5ce85541c8c16bc8486a5f6
Sudhersan Srinivasan Kunnavakkam Vinchimoor <94358947+sudhersankv@users.noreply.github.com>
2026-04-19 23:47:18 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorteddict = {}
+        for i in range(len(strs)):
+            key = "".join(sorted(strs[i]))
+            if key in sorteddict:
+                sorteddict[key].append(strs[i])
+            else:
+                sorteddict[key] = [strs[i]]
+        return list(sorteddict.values())
