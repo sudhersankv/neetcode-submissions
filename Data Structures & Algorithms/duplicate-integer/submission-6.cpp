294547b6acb00ae3f9634878cfdd4e641cadde7a
Sudhersan Srinivasan Kunnavakkam Vinchimoor <94358947+sudhersankv@users.noreply.github.com>
2026-08-12 23:29:27 -0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        std::unordered_set<int> seen;
+        
+        for(const int& x : nums)
+        {
+            if(seen.find(x) != seen.end())
+            {
+                return true;
+            }
+
+            seen.insert(x);
+
+        }
+        return false;
+        
+    }
+};
